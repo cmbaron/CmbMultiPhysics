267189d12649ae936f137ac45b5467cbc1e701aa
cbaron <cbaron@3cb15e06-d003-0410-bd8b-fc3653724bf5>
2005-10-24 23:03:00 +0000

    adding drivables

---
+++ src/CmbMultiPhysics/Drivables/Car.java
@@ -0,0 +1,23 @@
+/*
+ * Car.java
+ *
+ * Created on October 24, 2005, 7:12 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package CmbMultiPhysics.Drivables;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Car {
+    
+    /** Creates a new instance of Car */
+    public Car() {
+    }
+    
+}
